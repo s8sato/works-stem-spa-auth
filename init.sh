18774287864621736423d2a203728bf65d6c33c5
satu-n <49983831+satu-n@users.noreply.github.com>
2020-12-09 20:31:40 +0900

    init.sh: group redirect

--- init.sh
+++ init.sh
@@ -15,30 +15,38 @@ readonly API_PORT="3000"
 personal_use=false
 
 if [ $# -ne 4 ]; then
-  echo "$# arguments found." 1>&2
-  echo "Specify exactly 4 arguments." 1>&2
+  {
+    echo "$# arguments found."
+    echo "Specify exactly 4 arguments."
+  } 1>&2
   exit 1
 fi
 
 dest=".env"
 : >$dest
-echo "UID=$(id -u $USER)" >>$dest
-echo "GID=$(id -g $USER)" >>$dest
-echo "APP_NAME=$1" >>$dest
-echo "DB_PASSWORD=$2" >>$dest
+{
+  echo "UID=$(id -u $USER)"
+  echo "GID=$(id -g $USER)"
+  echo "APP_NAME=$1"
+  echo "DB_PASSWORD=$2"
+} >>$dest
 
 dest="api/.env"
 : >$dest
-echo "APP_NAME=$1" >>$dest
-echo "DATABASE_URL=postgres://postgres:$2@db:5432/postgres" >>$dest
-echo "SPARKPOST_API_KEY=$3" >>$dest
-echo "SENDING_EMAIL_ADDRESS=$4" >>$dest
-echo "API_PROTOCOL=$API_PROTOCOL" >>$dest
-echo "API_HOST=$API_HOST" >>$dest
-echo "API_PORT=$API_PORT" >>$dest
+{
+  echo "APP_NAME=$1"
+  echo "DATABASE_URL=postgres://postgres:$2@db:5432/postgres"
+  echo "SPARKPOST_API_KEY=$3"
+  echo "SENDING_EMAIL_ADDRESS=$4"
+  echo "API_PROTOCOL=$API_PROTOCOL"
+  echo "API_HOST=$API_HOST"
+  echo "API_PORT=$API_PORT"
+} >>$dest
 
 dest="web/src/Config.elm"
 : >$dest
-echo "module Config exposing (..)" >>$dest
-echo "appName = \"$1\"" >>$dest
-echo "epBase = \"${API_PROTOCOL}://${API_HOST}:${API_PORT}/api\"" >>$dest
+{
+  echo "module Config exposing (..)"
+  echo "appName = \"$1\""
+  echo "epBase = \"${API_PROTOCOL}://${API_HOST}:${API_PORT}/api\""
+} >>$dest
